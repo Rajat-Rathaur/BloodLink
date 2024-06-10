fbb2d31eef03cefe05e886033a99101d9a8c8620
Rajat-Rathaur <142532314+Rajat-Rathaur@users.noreply.github.com>
2024-06-10 17:04:08 +0530

    changed frontend port

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
 	cors({
 		origin: [
-			"http://localhost:3000",
+			"http://localhost:3000", "https://bloodconnect-one.vercel.app"
 		],
 		credentials: true,
 	})
