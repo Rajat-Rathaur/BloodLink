35335825fab703e21335e4b2e8f84ab7337e412e
Rajat-Rathaur <142532314+Rajat-Rathaur@users.noreply.github.com>
2024-06-16 13:15:47 +0530

    changed backend to render

--- client/src/Components/Api.js
+++ client/src/Components/Api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3177/";
+/* const baseURL = "http://localhost:3177/"; */
+const baseURL = "https://bloodlink-1.onrender.com/";
 
 export default axios.create({ baseURL: baseURL });
